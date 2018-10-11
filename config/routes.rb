cdfd6754c76657eb495f0f1fb834476973a79641
Sahidur Suman <suman5040@gmail.com>
2018-10-11 22:12:30 +0600

    Added book resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+
+  root to: 'books#inicio'
+
+  get :prueba, to: 'books#inicio'
+  get :prueba2, to: 'books#prueba2'
 end
